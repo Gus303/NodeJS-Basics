173971337b4240925d109e6d12870463eecab52d
Gustavo Rocha <gurocha05@gmail.com>
2025-02-04 16:08:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const server = express();
 
+//http://localhost:3000/hello?name=Gustavo&age=19
+// Query params = ?name=Gustavo&age=19
+
+//http://localhost:3000/hello/Gustavo
+//Route params = /hello/:name
+
 server.get("/hello", (req, res) => {
+    const { name, age } = req.query;
+
     return res.json({ 
         title: "Hello World",
-        message: "Lorem ipsum"
+        message: `My name is ${name}`,
+        age: age
     });
 });
 
