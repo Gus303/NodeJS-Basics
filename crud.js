ec3c24aae8edb511939ea68ed62cc02899dd9b5c
Gustavo Rocha <gurocha05@gmail.com>
2025-02-07 16:06:13 -0300

    ajusting

--- crud.js
+++ crud.js
@@ -3,10 +3,16 @@ const server = express();
 
 server.use(express.json());
 
-let costumers = [
+let crudes = [
     { id: 1, name: "Google", site: "http://google.com"},
     { id: 2, name: "LinkedIn", site: "http://linkedin.com"},
     { id: 3, name: "Github", site: "http://github.com"}
 ];
+
+server.get("/crudes", (req, res) => {
+    return res.json(crudes);
+})
+
+
 
 server.listen(3000);
