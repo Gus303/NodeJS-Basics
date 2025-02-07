8bc4f06c846b0339080d1aff4d34680de5086b71
Gustavo Rocha <gurocha05@gmail.com>
2025-02-07 16:10:50 -0300

    Update crud.js

--- crud.js
+++ crud.js
@@ -13,6 +13,12 @@ server.get("/crudes", (req, res) => {
     return res.json(crudes);
 })
 
+server.get("/crudes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const crud = crudes.find(item => item.id === id);
+    const status = crudes ? 200 : 404;
 
+    return res.status(status).json(crud);
+})
 
 server.listen(3000);
