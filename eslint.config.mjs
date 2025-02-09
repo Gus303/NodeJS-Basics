dbdc992331f1c3b6e6ce8e0ec29827adeca0e783
Gustavo Rocha <gurocha05@gmail.com>
2025-02-09 17:54:57 -0300

    aaaaaaaaaaaaaaaaaaaaa

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": ["error", {
+            "vars": "all",
+            "args": "after-used",
+            "caughtErrors": "all",
+            "ignoreRestSiblings": false,
+            "reportUsedIgnorePattern": false
+        }]
+    },
+  },
+];
