d8ebaba0d7516c992eb05e81c04e4c8c309592a4
Gustavo Rocha <gurocha05@gmail.com>
2025-02-10 13:41:31 -0300

    single quote

--- src/app/controllers/CustomersControllers.js
+++ src/app/controllers/CustomersControllers.js
@@ -1,7 +1,7 @@
 let customers =[
-    { id: 1, name: 'Google', site: 'http://google.com'},
-    { id: 2, name: 'LinkedIn', site: 'http://linkedin.com'},
-    { id: 3, name: 'Github', site: 'http://github.com'}
+    { id: 1, name: "Google", site: "http://google.com"},
+    { id: 2, name: "LinkedIn", site: "http://linkedin.com"},
+    { id: 3, name: "Github", site: "http://github.com"}
 ];
 class CustomersControllers {
 
@@ -12,32 +12,32 @@ class CustomersControllers {
         const id = parseInt(req.params.id);
         const customer = customers.find(item => item.id === id);
         const status = customer ? 200 : 404;
-    
+
         console.debug('GET :: /customers/:id', customer);
-        
-    
+
+
         return res.status(status).json(customer);
     }
     create(req, res){
         const { name, site } = req.body;
         const id = customers[customers.length - 1].id + 1;
-    
+
         const newCustomer = {id, name, site};
         customers.push(newCustomer);
-    
+
         return res.status(201).json(newCustomer);
     }
     update(req, res){
         const id = parseInt(req.params.id);
         const { name, site } = req.body;
-    
+
         const index = customers.findIndex(item => item.id === id);
         const status = index >= 0 ? 200 : 400;
-    
+
         if(index >= 0){
             customers[index] = {id: parseInt(id), name, site};
         }
-    
+
         return res.status(status).json(customers[index]);
     }
     destroy(req, res){
