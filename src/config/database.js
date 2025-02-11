c00cd2ac7be0346149e3090445617cb264f18d55
Gustavo Rocha <gurocha05@gmail.com>
2025-02-11 14:49:36 -0300

    database save

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'secret',
-  database: 'data-nodejs',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   define: {
     timestamp: true,
     underscored: true,
--- src/database/migrations/20250211170727-create-customers.js
+++ src/database/migrations/20250211170727-create-customers.js
@@ -1,9 +1,33 @@
 module.exports = {
-up (queryInterface, Sequelize) {
-
+up: (queryInterface, Sequelize) => {
+  return queryInterface.createTable('customers', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    create_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    update_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
   },
 
-down (queryInterface, Sequelize) {
-
-  }
+down: queryInterface => {
+  return queryInterface.dropTable('customers');
+  },
 };
